7969afe3e7b118e8c3524a3b28e125f7b91c5859
hkhk366 <hkhk366@qq.com>
2016-03-23 19:11:28 -0400

    1
    1

---
+++ 82. Remove Duplicates from Sorted List II.cpp
@@ -0,0 +1,52 @@
+﻿//Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the original list.
+//
+//For example,
+//Given 1->2->3->3->4->4->5, return 1->2->5.
+//Given 1->1->1->2->3, return 2->3.
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+ListNode *deleteDuplicates(ListNode *head) {
+	if (!head || !head->next) return head;
+	ListNode *result = NULL; ListNode **p = &result;
+	while (head)
+	{
+		if (head->next && head->val==head->next->val)
+		{
+			int temp = head->val;
+			while (head && head->val == temp)
+				head = head->next;
+		}
+		else
+		{
+			*p = head; p = &((*p)->next);
+			head = head->next;
+		}
+		
+	}
+	*p = NULL;
+	return result;
+}
+
+
+void main()
+{
+
+}
---
+++ 83. Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,56 @@
+﻿//Given a sorted linked list, delete all duplicates such that each element appear only once.
+//
+//For example,
+//Given 1->1->2, return 1->2.
+//Given 1->1->2->3->3, return 1->2->3.
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+class Solution {
+public:
+	ListNode* deleteDuplicates(ListNode* head) {
+		if (!head || !head->next)
+			return head;
+		ListNode *p = head;
+		while (p && p->next)
+		{
+			if (p->val == p->next->val)
+				p->next = p->next->next;
+			else
+				p = p->next;
+
+
+		}
+		return head;
+
+
+
+	}
+};
+
+
+void main()
+{
+	ListNode *test = new ListNode(1);
+	test->next = new ListNode(1);
+	test->next->next = new ListNode(1);
+	deleteDuplicates(test);
+}
