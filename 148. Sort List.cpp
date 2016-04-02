dbba9d997380cbcb226c45a4a068f2dddf9e17aa
hkhk366 <hkhk366@qq.com>
2016-04-02 16:43:30 -0400

    1
    1

---
+++ 148. Sort List.cpp
@@ -0,0 +1,74 @@
+﻿//Sort a linked list in O(n log n) time using constant space complexity.
+//
+//Subscribe to see which companies asked this question
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+#include <list>
+
+using namespace std;
+
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+ListNode *merge(ListNode *head, ListNode *head2)
+{
+	ListNode *result = new ListNode(-1), *var = result;
+	while (head || head2)
+	{
+		if (head && (!head2 || head->val <= head2->val))
+		{
+			var->next = head;
+			head = head->next;
+			var = var->next;
+		}
+		if (head2 && (!head || head2->val < head->val))
+		{
+			var->next = head2;
+			head2 = head2->next;
+			var = var->next;
+		}
+		
+
+
+	}
+	var->next = nullptr;
+	return result->next;
+}
+
+ListNode* sortList(ListNode* head) {
+	if (!head || !head->next)
+		return head;
+	ListNode *fast = head->next, *slow = head, *head2 = nullptr;
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	head2 = slow->next;
+	slow->next = nullptr;
+	return merge(sortList(head), sortList(head2));
+
+}
+
+
+void main()
+{
+	ListNode *test = new ListNode(3);
+	test->next = new ListNode(2);
+	test->next->next = new ListNode(4);
+	sortList(test);
+
+}
---
+++ 150. Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,57 @@
+﻿//Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+//
+//Valid operators are + , -, *, / .Each operand may be an integer or another expression.
+//
+//Some examples :
+//["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+//["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+//Subscribe to see which companies asked this question
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+#include <list>
+
+using namespace std;
+
+class Solution {
+public:
+	int evalRPN(vector<string>& tokens) {
+		stack<int> stack;
+		for (auto s : tokens)
+		{
+			if (s.size() > 1 || isdigit(s[0]))
+				stack.push(stoi(s));
+			else
+			{
+				auto op1 = stack.top(); stack.pop();
+				auto op2 = stack.top(); stack.pop();
+				switch (s[0])
+				{
+				case '+': stack.push(op2 + op1); break;
+				case '-': stack.push(op2 - op1); break;
+				case '*': stack.push(op2*op1); break;
+				case '/': stack.push(op2 / op1); break;
+				}
+
+			}
+
+		}
+		return stack.top();
+
+
+	}
+};
+
+
+
+void main()
+{
+
+}
