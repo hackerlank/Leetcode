bb0c73bfa7f84ffaef151b08b7748517dd8b0768
hkhk366 <hkhk366@qq.com>
2016-04-02 00:12:59 -0400

    1
    1

---
+++ 144. Binary Tree Preorder Traversal.cpp
@@ -0,0 +1,75 @@
+﻿//Given a binary tree, return the preorder traversal of its nodes' values.
+//
+//For example :
+//Given binary tree{ 1, #, 2, 3 },
+//1
+//\
+//2
+///
+//3
+//return[1, 2, 3].
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+#include <list>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+void recursive(TreeNode* root,vector<int> &result)
+{
+	if (!root)
+		return;
+	result.push_back(root->val);
+	if (root->left)
+		recursive(root->left, result);
+	if (root->right)
+		recursive(root->right, result);
+}
+
+vector<int> preorderTraversal2(TreeNode* root) {
+	if (!root)
+		return vector<int>();
+	vector<int> result;
+	recursive(root, result);
+	return result;
+}
+
+vector<int> preorderTraversal(TreeNode* root) {
+	if (!root)
+		return vector<int>();
+	stack<TreeNode *> s;
+	vector<int> result;
+	s.push(root);
+	while (!s.empty())
+	{
+		TreeNode *temp = s.top();
+		result.push_back(temp->val);
+		s.pop();
+		if (temp->right)
+			s.push(temp->right);
+		if (temp->left)
+			s.push(temp->left);
+	}
+	return result;
+}
+void main()
+{
+	TreeNode *test = new TreeNode(1);
+	test->left = new TreeNode(2);
+	preorderTraversal(test);
+}
---
+++ 147. Insertion Sort List.cpp
@@ -0,0 +1,55 @@
+﻿//Sort a linked list using insertion sort.
+//
+//Subscribe to see which companies asked this question
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+#include <numeric>
+#include <stack>
+#include <map>
+#include <list>
+
+using namespace std;
+
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+
+
+ListNode *insertionSortList(ListNode *head) {
+	if (!head)
+		return nullptr;
+	ListNode *result = new ListNode(-1);
+	while (head)
+	{
+		ListNode *preuslt = result;
+		ListNode *next = head->next;
+		while (preuslt->next && preuslt->next->val<head->val)
+			preuslt = preuslt->next;
+		head->next = preuslt->next;
+		preuslt->next = head;
+		head = next;
+	}
+	return result->next;
+}
+
+
+void main()
+{
+	ListNode *test = new ListNode(5);
+	insertionSortList(test);
+	test->next = new ListNode(3);
+	test->next->next = new ListNode(7);
+	test->next->next->next = new ListNode(4);
+	test->next->next->next->next = new ListNode(2);
+	insertionSortList(test);
+}
